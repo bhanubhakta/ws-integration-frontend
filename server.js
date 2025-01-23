39bf8690ff74efcee0460aafc4622f5fa8c453c5
Bhanu Bhakta Sigdel <sigdel.bhanu29@gmail.com>
2025-01-22 23:47:00 -0700

    More params

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 // server.js
+const argv = require('optimist')
+            .usage('Usage: $0 -t [Token] -p [Port]')
+            .demand(['t', 'p'])
+            .argv;
+
+const jsonata = require('jsonata');
+const fs = require('fs'); // Import the file system module
+
 const express = require('express');
 const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-const port = 3003;
+const port = argv.p;
 
 // Serve static files (HTML) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,7 +22,26 @@ const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyMWYxZWY2OC1mNWViLTQ0YWYtOGI5OC0wYzk0ZDgyZjQzMzEiLCJzdWIiOiI0Iiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNzM3MzUyMjk2LCJleHAiOjE3MzczNTQwOTZ9.3Otc5bNnlXmxRN4eXa5xadDQJM4YaBfNmLOcclXKXjQ';
+const token = argv.t;
+
+const data = {
+  example: [
+    {value: 10},
+    {value: 4},
+    {value: 20},
+  ]
+};
+
+fs.readFile('test.jsonata', 'utf8', (err, fileData) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const expression = jsonata(fileData.trim());
+  expression.evaluate(data).then((result) => {
+  console.log("Result:", result);
+});
+});
 
 // const cable = createConsumer(`ws://localhost:3001/cable?token=${token}`,
 //   {
